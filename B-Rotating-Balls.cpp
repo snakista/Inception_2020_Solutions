cdb458e4229d977c3321e157da00b9c153c94900
Sitanshu Yadav <sitanshyadav@gmail.com>
2020-09-21 21:16:02 +0530

    Create B-Rotating-Balls.cpp

---
+++ B-Rotating-Balls.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define pb push_back
+#define mk make_pair
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+		ll n;
+		cin>>n;
+		if(n%2==1)
+		{
+			cout<<"Yes"<<'\n';
+		}
+		else 
+		cout<<"No"<<'\n';
+	}
+}
